aa7e4e68e7e59dda6d2b13c73a3a2c1b3c90a295
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-10-08 18:39:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import passlib.hash
 import numpy as np
 
 # Crear DataFrames para almacenar datos de partidos y jugadores
@@ -8,7 +8,7 @@ partidos_df = pd.DataFrame(columns=["Fecha", "Equipo Local", "Equipo Visitante",
 jugadores_df = pd.DataFrame(columns=["Nombre", "Posición"])
 
 # Crear un diccionario para almacenar usuarios y contraseñas (solo como ejemplo, debe ser más seguro en la implementación real)
-usuarios = {"usuario1": "clave1", "usuario2": "clave2"}
+usuarios = {"usuario1": "hash1", "usuario2": "hash2"}  # Las contraseñas están hasheadas
 
 # Iniciar sesión
 def iniciar_sesion():
@@ -17,7 +17,7 @@ def iniciar_sesion():
     clave = st.text_input("Clave", type="password")
 
     if st.button("Iniciar Sesión"):
-        if usuario in usuarios and usuarios[usuario] == clave:
+        if usuario in usuarios and passlib.hash.verify(clave, usuarios[usuario]):
             st.success("Inicio de sesión exitoso.")
             return True
         else:
@@ -35,7 +35,9 @@ def registrar_usuario():
         if nuevo_usuario in usuarios:
             st.warning("El usuario ya existe. Elije otro nombre de usuario.")
         else:
-            usuarios[nuevo_usuario] = nueva_clave
+            # Hashear la contraseña antes de almacenarla
+            hash_clave = passlib.hash.sha256_crypt.hash(nueva_clave)
+            usuarios[nuevo_usuario] = hash_clave
             st.success("Registro exitoso. Ahora puedes iniciar sesión.")
 
 # Comprobar si el usuario ha iniciado sesión o desea registrarse
