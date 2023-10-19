b51af44f35cad3def4b260f86b1be43f4010b290
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-10-19 16:13:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -117,8 +117,7 @@ if get_current_user() is not None:
         fecha = st.date_input("Fecha del Partido")
         # Obtener los nombres de los equipos desde el DataFrame equipos_df
         username = get_current_user()  # Obtiene el nombre de usuario actual
-        equipos = list(equipos_df['Equipo'])
-        equipo_local = st.selectbox("Equipo Local", equipos)
+        equipos = list(equipos_df[equipos_df['Username'] == username]['Equipo'])        equipo_local = st.selectbox("Equipo Local", equipos)
         equipo_visitante = st.selectbox("Equipo Visitante", equipos)
         goles_local = st.number_input("Goles del Equipo Local", step=1)
         goles_visitante = st.number_input("Goles del Equipo Visitante", step=1)
