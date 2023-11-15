4c9e21f17d5965e9c26e50ea61795679fc5d6eeb
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-11-15 17:42:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -101,9 +101,9 @@ if get_current_user() is not None:
         st.subheader("Registro de Partido")
         fecha = st.date_input("Fecha del Partido")
         # Obtener los nombres de los equipos desde el DataFrame equipos_df
-        nombres_equipos = [equipo['Equipo'] for equipo in equipos_user]
-        equipo_local = st.selectbox("Equipo Local", nombres_equipos)
-        equipo_visitante = st.selectbox("Equipo Visitante", nombres_equipos)
+        nombres_equipos = pd.DataFrame(equipos_user)
+        equipo_local = st.selectbox("Equipo Local", nombres_equipos['Equipo'])
+        equipo_visitante = st.selectbox("Equipo Visitante", nombres_equipos['Equipo'])
         goles_local = st.number_input("Goles del Equipo Local", step=1)
         goles_visitante = st.number_input("Goles del Equipo Visitante", step=1)
 
